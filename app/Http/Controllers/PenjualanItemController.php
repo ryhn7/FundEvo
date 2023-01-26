99482e6c20839dd85bf4301f9c6c1e0ce1b67a42
hanif nurul fahmi <fahmismeba@gmail.com>
2023-01-26 11:17:35 +0700

    kerangka controller

---
+++ app/Http/Controllers/PenjualanItemController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PenjualanItemListrik;
+use Illuminate\Http\Request;
+
+class PenjualanItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PenjualanItemListrik  $penjualanItemListrik
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PenjualanItemListrik $penjualanItemListrik)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PenjualanItemListrik  $penjualanItemListrik
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PenjualanItemListrik $penjualanItemListrik)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PenjualanItemListrik  $penjualanItemListrik
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PenjualanItemListrik $penjualanItemListrik)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PenjualanItemListrik  $penjualanItemListrik
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PenjualanItemListrik $penjualanItemListrik)
+    {
+        //
+    }
+}

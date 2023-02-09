88cac07eeafb0ae1369ac43fa943805bdc6b884d
Rayhan Illyas <rayhan205732@gmail.com>
2023-02-09 09:46:22 +0700

    progresss for upload nota (view)

--- app/Http/Controllers/PengeluaranOpsBBMController.php
+++ app/Http/Controllers/PengeluaranOpsBBMController.php
@@ -6,6 +6,7 @@ use App\Models\PengeluaranOpsBBM;
 use App\Models\BBM;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PengeluaranOpsBBMController extends Controller
 {
@@ -55,6 +56,11 @@ class PengeluaranOpsBBMController extends Controller
      */
     public function store(Request $request)
     {
+
+        dd(
+            $images = $request->file('nota')
+        );
+
         $validated = $request->validate([
             'harga_penebusan_bbm' => 'nullable|numeric',
             'pph' => 'nullable|numeric',
@@ -70,8 +76,20 @@ class PengeluaranOpsBBMController extends Controller
             'pbb' => 'nullable|numeric',
             'biaya_lain' => 'nullable|numeric',
             'keterangan' => 'nullable',
-            'nota' => 'nullable',
+            'nota' => 'nullable|max:2048',
         ]);
+
+        if ($request->file('nota')) {
+            $images = $request->file('nota');
+            $imagesName = [];
+            foreach ($images as $image) {
+                $imageName = Str::random(10) . '.' . $image->extension();
+                $image->move(public_path('uploads'), $imageName);
+                $imagesName[] = $imageName;
+            }
+            dd($imagesName);
+            $validated['nota'] = $imagesName;
+        }
 
         PengeluaranOpsBBM::create($validated);
 

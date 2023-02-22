cfe01c29f401831cc03be16baeed85252100475e
Vito Ahmad <117362696+vhzrd@users.noreply.github.com>
2023-02-22 20:47:56 +0700

    add kartu laporan finansial v1

--- resources/views/tes.blade.php
+++ resources/views/tes.blade.php
@@ -1,83 +1,32 @@
 @extends('layouts.main')
 
 @section('container')
-    <div class="flex-auto px-0 pt-0 pb-2">
-        <div class="p-0 overflow-x-auto">
-            <table class="items-center w-full mb-0 align-top border-gray-200 text-slate-500">
-                    <tr>
-                        <th
-                            class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                            @sortablelink('created_at', 'Tanggal')
-                        </th>
-                        <th
-                            class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                            @sortablelink('stock_awal', 'Pertalite')</th>
-                        <th
-                            class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                            @sortablelink('penerimaan', 'Pertamax')</th>
-                        <th
-                            class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                            @sortablelink('tera_densiti', 'Solar')</th>
-                        <th
-                            class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                            @sortablelink('penjualan', ' Bio Solar')</th>
-                        <th
-                            class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                            @sortablelink('penjualan', ' Total Penebusan')</th>
-                        <th
-                            class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                            @sortablelink('penjualan', ' PPH')</th>
-                    </tr>
-                <tbody>
-                    <tr>
-                        <td class="p-2 align-middle bg-transparent border-b-0 whitespace-nowrap shadow-transparent">
-                            <div class="flex px-2 py-1">
-                                <div class="flex flex-col justify-center">
-                                    <h6 class="ml-2 mb-0 font-semibold leading-tight text-xs text-slate-400">
-                                        {{-- {{ $sells[0]->created_at->format('m/d/Y') }} --}} 05-06-2023
-                                    </h6>
+<div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+                <div class="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
+                    <div class="flex-auto p-4">
+                        <div class="flex flex-row -mx-3">
+                            <div class="flex-none w-2/3 max-w-full px-3">
+                                <div>
+                                    <p class="mb-0 font-open font-semibold leading-normal text-sm">
+                                        Total
+                                    </p>
+                                    <h5 class="mb-0 font-bold">
+                                        kdsn</h5>
+                                    <span
+                                        class="leading-normal text-sm font-weight-bolder text-lime-500">
+                                        Liter</span>
+                                </div>
+                                <span class="leading-normal text-sm font-weight-bolder text-lime-500">
+                                    Liter</span>
+                            </div>
+                            <div class="px-3 text-right basis-1/3">
+                                <div
+                                    class="inline-block w-12 h-12 text-center rounded-lg bg-gradient-to-tl from-[#060764] to-[#00b7dd]">
+                                    <i class="ni leading-none ni-money-coins text-lg relative top-3.5 text-white"></i>
                                 </div>
                             </div>
-                        </td>
-                        <td
-                            class="p-2 text-center align-middle bg-transparent border-b-0 whitespace-nowrap shadow-transparent">
-                            <p class="mb-0 font-semibold leading-tight text-xs">
-                                {{-- {{ $sells[0]->stock_awal }} --}}8000
-                            </p>
-                        </td>
-                        <td
-                            class="p-2 text-center align-middle bg-transparent border-b-0 whitespace-nowrap shadow-transparent">
-                            <span
-                                class="font-semibold leading-tight text-xs text-slate-400"></span>
-                        </td>
-                        <td
-                            class="p-2 text-center align-middle bg-transparent border-b-0 whitespace-nowrap shadow-transparent">
-                            <span class="font-semibold leading-tight text-xs text-slate-400">
-                                {{-- @if ($sells[0]->tera_densiti)
-                                    {{ $sells[0]->tera_densiti }}
-                                @else
-                                    -
-                                @endif --}}800
-                            </span>
-                        </td>
-                        <td
-                            class="p-2 text-center align-middle bg-transparent border-b-0 whitespace-nowrap shadow-transparent">
-                            <span
-                                class="font-semibold leading-tight text-xs text-slate-400"></span>
-                        </td>
-                        <td
-                            class="p-2 text-center align-middle bg-transparent border-b-0 whitespace-nowrap shadow-transparent">
-                            <span
-                                class="font-semibold leading-tight text-xs text-slate-400">100000000</span>
-                        </td>
-                        <td
-                            class="p-2 text-center align-middle bg-transparent border-b-0 whitespace-nowrap shadow-transparent">
-                            <span
-                                class="font-semibold leading-tight text-xs text-slate-400">150000</span>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 @endsection

4141cbcde2d5d6a6b02ec213dd1ef4d39e66ba96
Vito Ahmad <117362696+vhzrd@users.noreply.github.com>
2023-05-23 21:44:57 +0700

    checkpoint frontend

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -40,7 +40,7 @@
                                     <div class="mt-3 ml-2.5 text-sm text-red-700">{{ session('loginError') }}</div>
                                 @endif
                                 <button
-                                    class="mt-5 inline-block px-6 py-3 font-bold text-center text-white uppercase align-middle transition-all bg-transparent rounded-full cursor-pointer leading-pro text-xs ease-soft-in shadow-soft-md bg-150 bg-gradient-to-tl from-red-500 to-yellow-400 hover:shadow-soft-xs active:opacity-85 hover:scale-102 tracking-tight-soft bg-x-25">Masuk</button>
+                                    class="mt-7.5 inline-block px-6 py-3 font-bold text-base text-center text-white uppercase align-middle transition-all bg-transparent rounded-full cursor-pointer leading-pro text-xs ease-soft-in shadow-soft-md bg-150 bg-gradient-to-tl from-red-500 to-yellow-400 hover:shadow-soft-xs active:opacity-85 hover:scale-102 tracking-tight-soft bg-x-25">Masuk</button>
                             </div>
                         </form>
                     </div>
